32e85f9f89da050fbe78f00cd62e010bc676bf9c
Lyubomir Dimitrov <lyubomir.dimitrov.8702@gmail.com>
2023-11-08 10:52:42 +0200

    add new files

---
+++ 07_set/ex_1.py
@@ -0,0 +1,59 @@
+my_set = set(input() for _ in range(int(input())))
+
+print(my_set)
+
+
+# print(my_set)
+# print(my_set.pop())
+
+# while my_set:
+#     print(my_set)
+#     print(my_set.pop())
+
+
+
+
+
+
+
+# n = int(input())
+# unic_list = []
+#
+# for _ in range(n):
+#     unic_list.append(input())
+#
+# unic_set = set(unic_list)
+# while unic_set:
+#     print(unic_set.pop())
+
+
+
+
+
+
+
+# n = int(input())
+# unic_list = []
+# for i in range(n):
+#     current_name = input()
+#     if current_name not in unic_list:
+#         unic_list.append(current_name)
+#
+# while unic_list:
+#     print(unic_list.pop())
+
+
+# n = int(input())
+# my_list = []
+# unic_list = []
+# for _ in range(n):
+#     current_name = input()
+#     my_list.append(current_name)
+#
+# for i in range(len(my_list)):
+#     current_n = my_list[i]
+#     if current_n not in unic_list:
+#         unic_list.append(current_n)
+#
+# while unic_list:
+#     print(unic_list.pop())
