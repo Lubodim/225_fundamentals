2dc93b7ded9b4e9f20221875220161d433295fdb
Lyubomir Dimitrov <lyubomir.dimitrov.8702@gmail.com>
2023-11-24 11:47:10 +0200

    add files

---
+++ 09_test/ex_0.pyi
@@ -0,0 +1,10 @@
+# my_dict = {
+#     "Ivan": {"matematika": {"Pyrwi_stok": 2, "Vtori_srok": 3},
+#              "Fizika": {"Pyrwi_stok": 2, "Vtori_srok": 3},
+#              "Programirane": {"Pyrwi_stok": 2, "Vtori_srok": 3},
+#              },
+#
+# }
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(my_list[4])
